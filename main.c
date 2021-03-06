18fb5916fb3dafb4f74c376eae2190330f1dd24d
Ruben S. Sevaldson <rubenss@stud.ntnu.no>
2021-03-06 20:26:59 +0100

    Before conforming to official solution

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ void print_mem() {
     }
     printf(" %d | addr: %p size: %d free: %d |",(int) ((uint8_t *)mem - (uint8_t*)prev), mem, mem->size, is_in_free_list);
     prev = mem;
-    mem = (struct mem_control_block *)((uint8_t *)(mem + 1) + mem->size);
+    mem = mem + 1 + mem->size;
     if (mem >= (struct mem_control_block *)(managed_memory_start + MEM_SIZE) || mem <= (struct mem_control_block *)managed_memory_start) {
       break;
     }
@@ -57,7 +57,7 @@ void print_list(struct mem_control_block *list) {
 void mymalloc_init() { 
   managed_memory_start = &heap;
   struct mem_control_block *m = (struct mem_control_block *)managed_memory_start;
-  m->size = MEM_SIZE - sizeof(struct mem_control_block);
+  m->size = (MEM_SIZE - sizeof(struct mem_control_block)) / sizeof(struct mem_control_block);
   m->next = (struct mem_control_block *)0;
   free_list_start = m;
   has_initialized = 1;
@@ -72,18 +72,20 @@ int round_up_to_multiple(int multiple, int num) {
 }
 
 void *mymalloc(long _numbytes) {
-  long numbytes = round_up_to_multiple(8, _numbytes);
+  /* long numbytes = round_up_to_multiple(8, _numbytes); */
+  long numunits = (_numbytes + sizeof(struct mem_control_block) - 1)/sizeof(struct mem_control_block);
+  printf("Allocating %lu bytes, which resolves to %lu units\n", _numbytes, numunits);
   if (has_initialized == 0) {
      mymalloc_init();
   }
 
   struct mem_control_block *curr_free_block = free_list_start;
-  int new_used_block_tot_size = numbytes + sizeof(struct mem_control_block);
+  int new_used_block_tot_size = numunits + 1;
   while(curr_free_block) {
     if (curr_free_block->size >= new_used_block_tot_size) {
-      void *curr_block_end_addr = (uint8_t *)curr_free_block + sizeof(struct mem_control_block) + curr_free_block->size;
+      struct mem_control_block *curr_block_end_addr = curr_free_block + 1 + curr_free_block->size;
       struct mem_control_block *new_used_block = (struct mem_control_block *)(curr_block_end_addr - new_used_block_tot_size);
-      new_used_block->size = numbytes;
+      new_used_block->size = numunits;
       curr_free_block->size -= new_used_block_tot_size;
       return new_used_block + 1;
     }
@@ -116,13 +118,13 @@ void myfree(void *firstbyte) {
   }
 
   // merge left
-  if (prev != NULL && (uint8_t *)prev + prev->size + sizeof(struct mem_control_block) == (uint8_t *)firstblock) {
+  if (prev != NULL && prev + prev->size + 1 == firstblock) {
     prev->next = firstblock->next;
-    prev->size += firstblock->size + sizeof(struct mem_control_block); 
+    prev->size += firstblock->size + 1;
   }
   // merge right
-  if (firstblock->next != NULL && (uint8_t *)firstblock + firstblock->size + sizeof(struct mem_control_block) == (uint8_t *)firstblock->next) {
-    firstblock->size += firstblock->next->size + sizeof(struct mem_control_block);
+  if (firstblock->next != NULL && firstblock + firstblock->size + 1 == firstblock->next) {
+    firstblock->size += firstblock->next->size + 1;
     firstblock->next = firstblock->next->next;
   }
 }
@@ -130,31 +132,31 @@ void myfree(void *firstbyte) {
 int main(int argc, char **argv) {
   void *p1 = mymalloc(16); 
   printf("Starting mem alloc. Start of memory is %p\n", managed_memory_start);
-  void *p2 = mymalloc(23); 
-  void *p3 = mymalloc(53); 
-  printf("SIZE %d\n", ((struct mem_control_block*)(p3) - 1)->size);
-  void *p4 = mymalloc(492); 
+  /* void *p2 = mymalloc(23); */ 
+  /* void *p3 = mymalloc(53); */ 
+  /* printf("SIZE %d\n", ((struct mem_control_block*)(p3) - 1)->size); */
+  /* void *p4 = mymalloc(492); */ 
+  /* print_mem(); */
+  /* myfree(p3); */
   print_mem();
-  myfree(p3);
-  print_mem();
 
 
-  // 53 is rounded up to 56
-  if (free_list_start->next == NULL || free_list_start->next->size != 56) {
-    if (free_list_start->next == NULL) {
-      printf("FAIL: expected free_list_start->next->size to be 56, but free_list_start->next was NULL\n");
-    } else {
-      printf("FAIL: expected free_list_start->next->size to be 56, but got %d\n", free_list_start->next->size);
-    }
-  }
+  /* // 53 is rounded up to 56 */
+  /* if (free_list_start->next == NULL || free_list_start->next->size != 56) { */
+  /*   if (free_list_start->next == NULL) { */
+  /*     printf("FAIL: expected free_list_start->next->size to be 56, but free_list_start->next was NULL\n"); */
+  /*   } else { */
+  /*     printf("FAIL: expected free_list_start->next->size to be 56, but got %d\n", free_list_start->next->size); */
+  /*   } */
+  /* } */
 
 
-  void *p = mymalloc(16);
-  struct mem_control_block* p_mcb = (struct mem_control_block*)((uint8_t *)p - sizeof(struct mem_control_block));
-  printf("addr %p, size: %d\n",p, p_mcb->size);
-  if (p_mcb->size != 16) {
-    printf("ERROR: Did not get 16 bytes");
-  }
+  /* void *p = mymalloc(16); */
+  /* struct mem_control_block* p_mcb = (struct mem_control_block*)((uint8_t *)p - sizeof(struct mem_control_block)); */
+  /* printf("addr %p, size: %d\n",p, p_mcb->size); */
+  /* if (p_mcb->size != 16) { */
+  /*   printf("ERROR: Did not get 16 bytes"); */
+  /* } */
 
   /* add your test cases here! */
 }
